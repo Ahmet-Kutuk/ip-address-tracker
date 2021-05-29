5fc46bb089cf920b1425b4f0b5e22d3b103ce380
Ahmet-Kutuk <ahmetcankutk@gmail.com>
2021-05-29 19:15:33 +0300

    ınput component done

--- src/components/Header/input/index.js
+++ src/components/Header/input/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Input() {
     return (
-        <div>
-            
+        <div className="Input">
+            <input className="Input-text" type="text" placeholder="Search for any IP address" />
+            <button className="Input-button">
+            <i class="fas fa-chevron-right"></i>
+            </button>
         </div>
     )
 }
