2551b7d92fb1a75b3c815a172d3ac1505c31130c
Ahmet-Kutuk <ahmetcankutk@gmail.com>
2021-05-29 21:37:51 +0300

    usermap marker icon added

--- src/components/Usermap/index.js
+++ src/components/Usermap/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 function Usermap() {
-    const position = [51.505, -0.09]
+    const position = [51.505, -0.09];
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
     return (
         <MapContainer style={{height:"700px",zIndex:"-1"}} center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
